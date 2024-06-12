169c54db2e642c093d81b3febca14571e8c4551b
mioe <mioe.boss@gmail.com>
2024-06-12 23:51:51 +0300

    init main file

--- src/change-case.rs
+++ src/change-case.rs
@@ -1,3 +1,12 @@
-fn main() {
-	println!("Hello, world!");
+use zed_extension_api as zed;
+
+struct ChangeCase {
 }
+
+impl zed::Extension for ChangeCase {
+	fn new() -> Self();
+
+	println!("hello world");
+}
+
+zed::register_extension!(ChangeCase);
